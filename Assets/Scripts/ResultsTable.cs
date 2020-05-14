88f8e1dcfc888f7fc9dec05de7896c3d4cbac6f3
AndresSalinas97 <i52salia@uco.es>
2020-05-14 11:25:39 +0200

    Keeps track of the best score

--- Assets/Scripts/ResultsTable.cs
+++ Assets/Scripts/ResultsTable.cs
@@ -1,43 +1,112 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 # pragma warning disable 649  // To disable SerializeField warnings
 
+/// <summary>
+/// This class is in charge of showing the results table and all the elements
+/// inside it.
+/// </summary>
 public class ResultsTable : MonoBehaviour
 {
-    [SerializeField] Sprite coinBronze, coinSilver, coinGold;
+    /// <summary>
+    /// Minimum score to get the bronze coin.
+    /// </summary>
+    private const int BRONZE_MIN_SCORE = 5;
+
+    /// <summary>
+    /// Minimum score to get the silver coin.
+    /// </summary>
+    private const int SILVER_MIN_SCORE = 15;
+
+    /// <summary>
+    /// Minimum score to get the gold coin.
+    /// </summary>
+    private const int GOLD_MIN_SCORE = 30;
+
+    /// <summary>
+    /// Bronze coin sprite.
+    /// </summary>
+    [SerializeField] Sprite coinBronze;
+
+    /// <summary>
+    /// Silver coin sprinte.
+    /// </summary>
+    [SerializeField] Sprite coinSilver;
+
+    /// <summary>
+    /// Gold coin sprite.
+    /// </summary>
+    [SerializeField] Sprite coinGold;
+
+    /// <summary>
+    /// Image where the coin sprites will be loaded.
+    /// </summary>
     [SerializeField] Image coinImage;
+
+    /// <summary>
+    /// Text field to show the final score.
+    /// </summary>
     [SerializeField] Text finalScoreText;
 
-    void Start()
+    /// <summary>
+    /// Text field to show the best score.
+    /// </summary>
+    [SerializeField] Text bestScoreText;
+
+    /// <summary>
+    /// Start is called just before the first frame update.
+    /// </summary>
+    private void Start()
     {
-        EventsManager.instance.OnGameOver += SpawnResults;
+        // Hide the results table until it's time to show it.
+        this.gameObject.SetActive(false);
+
+        // Hide the coin image until it's time to show it.
+        coinImage.enabled = false;
+
+        EventsManager.instance.OnGameOver += ShowResultsTable;
     }
 
+    /// <summary>
+    /// OnDestroy is called when the game object is destroyed.
+    /// </summary>
     private void OnDestroy()
     {
-        EventsManager.instance.OnGameOver -= SpawnResults;
+        EventsManager.instance.OnGameOver -= ShowResultsTable;
     }
 
-    private void SpawnResults()
+    /// <summary>
+    /// Shows the results table and all the elements inside it.
+    /// </summary>
+    private void ShowResultsTable()
 	{
-        int finalScore = ScoreManager.instance.GetScore();
+        // Gets the final and best scores
+        int finalScore = ScoreManager.instance.GetCurrentScore();
+        int bestScore = ScoreManager.instance.GetBestScore();
 
-        if (finalScore < 5)
+        // Sets the corresponding coin
+        if (finalScore >= BRONZE_MIN_SCORE && finalScore < SILVER_MIN_SCORE)
         {
             coinImage.sprite = coinBronze;
+            coinImage.enabled = true;
         }
-        else if (finalScore >= 5 && finalScore < 10)
+        else if (finalScore >= SILVER_MIN_SCORE && finalScore < GOLD_MIN_SCORE)
         {
             coinImage.sprite = coinSilver;
+            coinImage.enabled = true;
         }
-        else
+        else if (finalScore >= GOLD_MIN_SCORE)
         {
             coinImage.sprite = coinGold;
+            coinImage.enabled = true;
         }
 
+        // Sets the finalScoreText and the bestScoreText
         finalScoreText.text = finalScore.ToString();
+        bestScoreText.text = bestScore.ToString();
+
+        // Shows the results table
+        this.gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,19 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
+/// <summary>
+/// This singleton class handles the score system, that is, keeps the current
+/// score and saves and loads the the best score.
+/// </summary>
 public class ScoreManager : MonoBehaviour
 {
+    /// <summary>
+    /// Name for the file where the best score will be saved.
+    /// </summary>
+    private const string FILE_NAME = "bestScore.dat";
+
+    /// <summary>
+    /// The one and only instance of the ScoreManager.
+    /// </summary>
     public static ScoreManager instance;
 
-    private int score = 0;
+    /// <summary>
+    /// The current score.
+    /// </summary>
+    private int currentScore;
 
-    private void Start()
-    {
-        EventsManager.instance.OnGameStarted += ResetScore;
-        EventsManager.instance.OnTubesCrossed += IncreaseScore;
-    }
+    /// <summary>
+    /// The best score.
+    /// </summary>
+    private int bestScore;
 
+    /// <summary>
+    /// The complete file path where the best score will be saved.
+    /// </summary>
+    private string filePath;
+
+    /// <summary>
+    /// Awake is called after all objects are initialized, before the game
+    /// starts. It sets the instance value to reference this instance or
+    /// destroys this object in case it's already set.
+    /// </summary>
     private void Awake()
     {
         if (ScoreManager.instance != null)
@@ -26,18 +50,96 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
-    public int GetScore()
+    /// <summary>
+    /// Start is called just before the first frame update.
+    /// </summary>
+    private void Start()
     {
-        return score;
+        EventsManager.instance.OnGameStarted += ResetScore;
+        EventsManager.instance.OnTubesCrossed += IncreaseScore;
+
+        filePath = Application.persistentDataPath + "/" + FILE_NAME;
+
+        LoadBestScore();
     }
 
+    /// <summary>
+    /// OnDestroy is called when the game object is destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+        EventsManager.instance.OnTubesCrossed -= IncreaseScore;
+        EventsManager.instance.OnGameStarted -= ResetScore;
+    }
+
+    /// <summary>
+    /// Returns the current score.
+    /// </summary>
+    public int GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    /// <summary>
+    /// Returns the best score.
+    /// </summary>
+    public int GetBestScore()
+    {
+        return bestScore;
+    }
+
+    /// <summary>
+    /// Increases the currenet score.
+    /// </summary>
     private void IncreaseScore()
     {
-        score++;
+        currentScore++;
+
+        if (currentScore > bestScore)
+        {
+            bestScore = currentScore;
+            SaveBestScore();
+        }
     }
 
+    /// <summary>
+    /// Resets the current score.
+    /// </summary>
     private void ResetScore()
     {
-        score = 0;
+        currentScore = 0;
+    }
+
+    /// <summary>
+    /// Loads the best score from the file.
+    /// </summary>
+    private void LoadBestScore()
+    {
+        if (File.Exists(filePath))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(filePath, FileMode.Open);
+
+            bestScore = (int)bf.Deserialize(file);
+
+            file.Close();
+        }
+        else
+        {
+            bestScore = 0;
+        }
+    }
+
+    /// <summary>
+    /// Saves the best score to the file.
+    /// </summary>
+    private void SaveBestScore()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = new FileStream(filePath, FileMode.Create);
+
+        bf.Serialize(file, bestScore);
+
+        file.Close();
     }
 }
