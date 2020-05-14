587edb5f1e51b839ec51a3199c7cca5d3778b98d
AndresSalinas97 <i52salia@uco.es>
2020-05-14 14:39:19 +0200

    Delay first tube pair spawn

--- Assets/Scripts/TubePairSpawner.cs
+++ Assets/Scripts/TubePairSpawner.cs
@@ -1,47 +1,100 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 # pragma warning disable 649  // To disable SerializeField warnings
 
+/// <summary>
+/// This class handles the spawn of new tube pairs.
+/// </summary>
 public class TubePairSpawner : MonoBehaviour
 {
-    private const float MIN_Y = -1.5f;
-    private const float MAX_Y = 2.5f;
-    private const float TIEMPO_ENTRE_SPAWN = 1.7f;
-    private const float POS_X_INICIAL = 6;
-    private const float POS_Z_INICIAL = 0;
+    /// <summary>
+    /// Minimum value for the random y initial position of the tube pair.
+    /// </summary>
+    private const float MIN_INIT_Y = -1.5f;
 
-    [SerializeField] GameObject filaTubo;
+    /// <summary>
+    /// Maximum value for the random y initial position of the tube pair.
+    /// </summary>
+    private const float MAX_INIT_Y = 2.5f;
 
-    void Start()
+    /// <summary>
+    /// Initial x position for the tube pair.
+    /// </summary>
+    private const float INIT_X = 6;
+
+    /// <summary>
+    /// Initial z position for the tube pair.
+    /// </summary>
+    private const float INIT_Z = 0;
+
+    /// <summary>
+    /// Time (in seconds) to wait before spawning a new tube pair.
+    /// </summary>
+    private const float TIME_GAP = 1.7f;
+
+    /// <summary>
+    /// Time (in seconds) that takes the first tube pair to be spawned.
+    /// </summary>
+    private const float FIRST_TUBE_DELAY = 0.1f;
+
+    /// <summary>
+    /// The TubePair object.
+    /// </summary>
+    [SerializeField] GameObject tubePair;
+
+    /// <summary>
+    /// Start is called just before the first frame update.
+    /// </summary>
+    private void Start()
     {
         EventsManager.instance.OnGameStarted += StartSpawning;
         EventsManager.instance.OnGameOver += StopSpawning;
     }
 
+    /// <summary>
+    /// OnDestroy is called when the game object is destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+        EventsManager.instance.OnGameStarted -= StartSpawning;
+        EventsManager.instance.OnGameOver -= StopSpawning;
+    }
+
+    /// <summary>
+    /// Starts the Spawn coroutine.
+    /// </summary>
     private void StartSpawning()
     {
         StartCoroutine("Spawn");
     }
 
+    /// <summary>
+    /// Stops the Spawn coroutine.
+    /// </summary>
     private void StopSpawning()
     {
         StopAllCoroutines();
     }
 
+    /// <summary>
+    /// Spawns a new TubePair every TIME_GAP seconds.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Spawn()
     {
+        yield return new WaitForSeconds(FIRST_TUBE_DELAY);
+
         while (true)
         {
-            float posY = Random.Range(MIN_Y, MAX_Y);
+            float init_y = Random.Range(MIN_INIT_Y, MAX_INIT_Y);
 
             Instantiate(
-                filaTubo,
-                new Vector3(POS_X_INICIAL, posY, POS_Z_INICIAL),
+                tubePair,
+                new Vector3(INIT_X, init_y, INIT_Z),
                 Quaternion.identity);
 
-            yield return new WaitForSeconds(TIEMPO_ENTRE_SPAWN);
+            yield return new WaitForSeconds(TIME_GAP);
 
             yield return null;
         }
